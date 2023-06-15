a9fc094cd74fe924dabf6797802cf6346dddd8b5
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-06-15 08:43:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (driversArray) {
+    return driversArray.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function (driversArray) {
+    return driversArray.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier){
+    return (fare) => fare * multiplier;
+}
+
+function createFareMultiplier(multiplier){
+    return (fare) => fare * multiplier;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(driversArray, returnDriversCallback) {
+    return returnDriversCallback(driversArray);
+}
